8950992fb65afc7f59a94a00923714ddba17ec91
rubenpanjaitan <rubenif313107@gmail.com>
2016-09-28 23:04:47 +0700

    Initial commit

---
+++ app/src/main/java/aplikasi/test/rubenfcpanjaitan/multipletab/Fragment2.java
@@ -0,0 +1,18 @@
+package aplikasi.test.rubenfcpanjaitan.multipletab;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by Hita Do on 27/09/2016.
+ */
+public class Fragment2 extends Fragment {
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.tab_fragment_2, container, false);
+    }
+}
